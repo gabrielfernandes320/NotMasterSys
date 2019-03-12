10a65efd5dc5135a6c723dd9642e3e5946841135
comp1 <comp1@lab.unesc.net>
2019-03-12 20:04:56 -0300

    Creation of initials models

---
+++ NotMasterSysCoral/src/model/Usuario.java
@@ -0,0 +1,30 @@
+package model;
+
+public class Usuario {
+
+	private String usuario;
+	private String perfil;
+	
+	
+	public Usuario(String usuario, String perfil) {
+		super();
+		this.usuario = usuario;
+		this.perfil = perfil;
+	}
+	public String getUsuario() {
+		return usuario;
+	}
+	public void setUsuario(String usuario) {
+		this.usuario = usuario;
+	}
+	public String getPerfil() {
+		return perfil;
+	}
+	public void setPerfil(String perfil) {
+		this.perfil = perfil;
+	}
+	
+	
+	
+
+}
