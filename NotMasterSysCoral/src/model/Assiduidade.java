eec46f46237bf6189b7dd757e5850559b138d429
comp1 <comp1@lab.unesc.net>
2019-03-12 20:31:12 -0300

    Creation of Assiduidade method

---
+++ NotMasterSysCoral/src/model/Assiduidade.java
@@ -0,0 +1,34 @@
+package model;
+import java.util.Date;
+
+public class Assiduidade {
+	
+	private int codigo_matricula;
+	private Date data_entrada;
+	
+	public Assiduidade(int codigo_matricula, Date data_entrada) {
+		super();
+		this.codigo_matricula = codigo_matricula;
+		this.data_entrada = data_entrada;
+	}
+
+	public int getCodigo_matricula() {
+		return codigo_matricula;
+	}
+
+	public void setCodigo_matricula(int codigo_matricula) {
+		this.codigo_matricula = codigo_matricula;
+	}
+
+	public Date getData_entrada() {
+		return data_entrada;
+	}
+
+	public void setData_entrada(Date data_entrada) {
+		this.data_entrada = data_entrada;
+	} 
+	
+	
+	
+	
+}
