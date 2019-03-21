894706570f4f81bb6120b765ce5beaed10669d05
Enzoj0se <enzo190100@gmail.com>
2019-03-21 18:05:01 -0300

    implement modalidadesDAO methods

--- NotMasterSysCoral/src/database/ModalidadesDAO.java
+++ NotMasterSysCoral/src/database/ModalidadesDAO.java
@@ -7,10 +7,13 @@ import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
+import model.Aluno;
 import model.Modalidades;
 
 public class ModalidadesDAO extends MasterDAO{
 	
+	// Cria as variaveis contendo o select a ser feito.
+	private String is_delete = "delete from modalidades where modalidade = ? and gradacoes = ?";
 	private String is_selectAll = "select * from modalidades order by modalidade";
 	private String is_select = "select * from modalidades where modalidade = ? and graduacao = ? order by modalidade";
 	private String is_insert = "INSERT INTO modalidades	"
@@ -23,9 +26,14 @@ public class ModalidadesDAO extends MasterDAO{
 								+"		DEFAULT,		"
 								+"		?"
 								+"	)";
+	private String is_update = "UPDATE public.modalidades\r\n" + 
+							"   SET modalidade=?, gradacoes=?";
+	
 	private PreparedStatement pst_selectAll;
 	private PreparedStatement pst_select;
 	private PreparedStatement pst_insert;
+	private PreparedStatement pst_update;
+	private PreparedStatement pst_delete;
 	
 	Connection io_connection;
 	
@@ -35,6 +43,9 @@ public class ModalidadesDAO extends MasterDAO{
 		pst_selectAll = connection.prepareStatement(is_selectAll);
 		pst_select = connection.prepareStatement(is_select);
 		pst_insert = connection.prepareStatement(is_insert);	
+		pst_delete = connection.prepareStatement(is_delete);
+		pst_update = connection.prepareStatement(is_update);
+		
 	}
 
 	@Override
@@ -51,31 +62,67 @@ public class ModalidadesDAO extends MasterDAO{
 			 arlModalidade.add(model);
 		 }
 				
-		return arlModalidade;;
+		return arlModalidade;
 	}
 
 	@Override
 	public Object Select(Object parameter) throws SQLException {
-		// TODO Auto-generated method stub
-		return null;
+		
+//		cria o objeto modaliidade
+		Modalidades modalidade = null;
+		
+		Set(pst_select, 1, ((Modalidades)parameter).getModalidade());
+		
+		ResultSet rst = pst_select.executeQuery();
+		if(rst.next()) {
+			modalidade = new Modalidades();
+			modalidade.setModalidade(rst.getString("modalidade"));
+			modalidade.setGraduacoes(rst.getString("graduacoes"));
+		}
+		return modalidade;
 	}
 
 	@Override
 	public void Update(Object parameter) throws SQLException {
-		// TODO Auto-generated method stub
+		pst_insert.clearParameters();
 		
+		Modalidades lo_modalidade = (Modalidades)parameter;
+		Set(pst_update, 1, lo_modalidade.getModalidade());
+		Set(pst_update, 2, lo_modalidade.getGraduacoes());
+		pst_update.execute();
+		
+		if (pst_update.getUpdateCount() > 0) {
+			io_connection.commit();
+		}
 	}
 
 	@Override
 	public void Insert(Object parameter) throws SQLException {
-		// TODO Auto-generated method stub
+		pst_insert.clearParameters();
 		
+		Modalidades lo_modalidade = (Modalidades)parameter;
+		Set(pst_insert, 1, lo_modalidade.getModalidade());
+		Set(pst_insert, 2, lo_modalidade.getGraduacoes());
+		pst_insert.execute();
+		
+		if (pst_insert.getUpdateCount() > 0) {
+			io_connection.commit();
+		}
 	}
 
 	@Override
-	public void Delete(Object parameter) throws SQLException {
-		// TODO Auto-generated method stub
-		
+	public int Delete(Object parameter) throws SQLException {
+		int affectedrows = 0;
+
+		Modalidades lo_modalidade = (Modalidades)parameter;
+
+        Set(pst_delete, 1, lo_modalidade.getModalidade());
+        Set(pst_delete, 2, lo_modalidade.getGraduacoes());
+
+        affectedrows = pst_delete.executeUpdate();
+
+        return affectedrows;
+
 	}
 	
 	
