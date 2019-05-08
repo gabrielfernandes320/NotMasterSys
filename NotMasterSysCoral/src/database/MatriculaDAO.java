4840c0e600979dbe92c18408f366eba39224b6b9
Guilherme de Nez Silvano <guisilvano@hotmail.com>
2019-05-07 22:45:41 -0300

    as vezes eu fico de pau duro do nada

--- NotMasterSysCoral/src/database/MatriculaDAO.java
+++ NotMasterSysCoral/src/database/MatriculaDAO.java
@@ -15,15 +15,18 @@ public class MatriculaDAO extends MasterDAO{
 
 	private String is_selectAll = "select * from matriculas order by codigo_aluno";
 	
-	private String is_insert = "INSERT INTO Matriculas "
-			+ "(codigo_matricula, codigo_aluno, data_matricula, dia_vencimento, data_encerramento)"
-			+ "VALUES (DEFAULT,?,?,?,?);";
+	private String is_insert = "INSERT INTO Matriculas"
+			+ " ( codigo_matricula, codigo_aluno, data_matricula, dia_vencimento, data_encerramento )"
+			+ " VALUES ( DEFAULT, ?, ?, ?, ? )";
 	
-	private String is_select = "select * from matriculas where codigo_aluno =? ";
+	private String is_select = "select * from matriculas where codigo_aluno = ?";
+	
+	private String is_selectAluno = "select * from alunos where aluno = ?";
 	
 	private PreparedStatement pst_selectAll;
 	private PreparedStatement pst_insert;
 	private PreparedStatement pst_select;
+	private PreparedStatement pst_selectAluno;
 	
 	Connection io_connection;
 	
@@ -32,7 +35,35 @@ public class MatriculaDAO extends MasterDAO{
 		io_connection = connection;
 		
 		pst_selectAll = connection.prepareStatement(is_selectAll);
+		pst_selectAluno = connection.prepareStatement(is_selectAluno);
 		pst_insert = connection.prepareStatement(is_insert);
+		
+	}
+	
+	public void Insert(Object parameter) throws SQLException {
+		
+		pst_insert.clearParameters();
+		
+		Matricula lo_matricula = (Matricula)parameter;
+		
+		Set(pst_insert, 1, lo_matricula.getCodigo_aluno());
+		Set(pst_insert, 2, lo_matricula.getData_matricula());
+		Set(pst_insert, 3, lo_matricula.getDia_vencimento());
+		
+		if (lo_matricula.getData_encerramento() != null) {
+			Set(pst_insert, 4, lo_matricula.getData_encerramento());
+		} else {
+			Set(pst_insert, 4, null);
+		}
+		
+		
+		pst_insert.execute();
+		
+		if (pst_insert.getUpdateCount() > 0) {
+			io_connection.commit();
+		}
+		
+		
 		
 	}
 	
@@ -52,55 +83,49 @@ public class MatriculaDAO extends MasterDAO{
 		return arlMatricula;
 	}
 	
-public void Insert(Object parameter) throws SQLException {
+
+	@Override
+	public Matricula Select(Object parameter) throws SQLException {
+		return null;
+		// TODO Auto-generated method stub
+
+	}
+	
+	
+	public Matricula SelectAluno(Object parameter) throws SQLException {
 		
-		pst_insert.clearParameters();
-		
+		Matricula matricula = null;
 		Matricula lo_matricula = (Matricula)parameter;
 		
-		//Set(pst_insert, 1, lo_matricula.getCodigo_matricula());
-		Set(pst_insert, 1, lo_matricula.getCodigo_aluno());
-		Set(pst_insert, 2, lo_matricula.getData_matricula());
-		Set(pst_insert, 3, lo_matricula.getDia_vencimento());
-		Set(pst_insert, 4, lo_matricula.getData_encerramento());
-
+		Set(pst_selectAluno, 1, lo_matricula.getAluno());
 		
-		System.out.println(lo_matricula.getCodigo_aluno());
+		ResultSet rst = pst_selectAluno.executeQuery();
 		
-		pst_insert.execute();
-		
-		if (pst_insert.getUpdateCount() > 0) {
-			io_connection.commit();
+		if (rst.next()) {
+			matricula = new Matricula();
+			matricula.setCodigo_aluno(rst.getInt("codigo_aluno"));
+			matricula.setAluno(rst.getString("aluno"));
+			
+			return matricula;			
 		}
 		
+		else
+			return null;
+		
 	}
-
-
-@Override
-public Object Select(Object parameter) throws SQLException {
-	// TODO Auto-generated method stub
-	Matricula matricula = null;
-	Matricula lo_matricula = (Matricula)parameter;	
 	
-	Set(pst_select, 1, lo_matricula.getCodigo_aluno());	
-	ResultSet rst = pst_select.executeQuery();
+	@Override
+	public void Update(Object parameter) throws SQLException {
+		// TODO Auto-generated method stub
+		
+	}
 	
-	return null;
-}
-
-
-@Override
-public void Update(Object parameter) throws SQLException {
-	// TODO Auto-generated method stub
 	
-}
-
-
-@Override
-public int Delete(Object parameter) throws SQLException {
-	// TODO Auto-generated method stub
-	return 0;
-}
+	@Override
+	public int Delete(Object parameter) throws SQLException {
+		// TODO Auto-generated method stub
+		return 0;
+	}
 
 
 }
