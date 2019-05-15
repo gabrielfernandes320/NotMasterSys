64b929a5f5cc1a808be178f95524d78ca4df4d66
Guilherme de Nez Silvano <guisilvano@hotmail.com>
2019-05-15 15:51:00 -0300

    asdf

--- NotMasterSysCoral/src/view/StudentSearchFrm.java
+++ NotMasterSysCoral/src/view/StudentSearchFrm.java
@@ -10,6 +10,7 @@ import javax.swing.ListSelectionModel;
 import database.AlunoDAO;
 import database.ConnectionFactory;
 import model.Aluno;
+import model.Matricula;
 import table.model.StudentsTableModel;
 import java.awt.event.ActionListener;
 import java.awt.event.MouseAdapter;
@@ -26,6 +27,80 @@ public class StudentSearchFrm extends JDialog {
 	Connection conn = ConnectionFactory.getConnection("master", "admin", "admin");
 	private JTextField txfSearch;
 
+	public StudentSearchFrm(final MatriculaFrm window) {
+
+		setBounds(100, 100, 471, 330);
+		getContentPane().setLayout(null);
+		{
+			JPanel buttonPane = new JPanel();
+			buttonPane.setBounds(10, 248, 434, 33);
+			getContentPane().add(buttonPane);
+			{
+				buttonPane.setLayout(null);
+
+				JLabel lblNewLabel = new JLabel("Duplo clique na linha para selecionar o aluno desejado");
+				lblNewLabel.setBounds(0, 9, 434, 14);
+				buttonPane.add(lblNewLabel);
+			}
+		}
+
+		txfSearch = new JTextField();
+		txfSearch.setBounds(89, 11, 220, 20);
+		getContentPane().add(txfSearch);
+		txfSearch.setColumns(10);
+
+		JLabel lblStudent = new JLabel("Aluno:");
+		lblStudent.setBounds(10, 13, 75, 17);
+		getContentPane().add(lblStudent);
+
+		JButton btnSearch = new JButton("Pesquisar");
+		btnSearch.setBackground(SystemColor.menu);
+		btnSearch.setBounds(312, 10, 89, 23);
+		getContentPane().add(btnSearch);
+		txfSearch.setText("");
+		JPanel painelFundo;
+
+		painelFundo = new JPanel();
+		model = new StudentsTableModel();
+		painelFundo.setLayout(null);
+		painelFundo.setBounds(10, 42, 434, 195);
+		getContentPane().add(painelFundo);
+
+		JTable tabela = new JTable(model);
+		tabela.setSelectionMode(ListSelectionModel.SINGLE_SELECTION);
+		JScrollPane barraRolagem = new JScrollPane(tabela);
+		barraRolagem.setBounds(0, 0, 434, 195);
+		painelFundo.add(barraRolagem);
+
+		tabela.addMouseListener(new MouseAdapter() {
+			public void mouseClicked(MouseEvent e) {
+				if (e.getClickCount() == 2) {
+
+					System.out.println(" double click");
+
+					Aluno p = new Aluno();
+					p.setCodigo_aluno(Integer.parseInt((String) tabela.getValueAt(tabela.getSelectedRow(), 0)));
+					try {
+						AlunoDAO AlunoDAO = new AlunoDAO(conn);
+						
+						p = AlunoDAO.Select(p);
+						Matricula mat = new Matricula();
+						
+						mat.setAluno(p.getAluno());
+						mat.setCodigo_aluno(p.getCodigo_aluno());
+						
+						window.updateFields(mat);
+					} catch (SQLException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+	
+					dispose();
+
+				}
+			}
+		});
+	
 	public StudentSearchFrm(final StudentFrm window) {
 
 		setBounds(100, 100, 471, 330);
